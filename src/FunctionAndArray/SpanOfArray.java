f187a4ea25f68c1716df517f72b107db4955565c
Osho <64656510+Osho957@users.noreply.github.com>
2021-07-05 20:55:04 +0530

    Update SpanOfArray.java

--- src/FunctionAndArray/SpanOfArray.java
+++ src/FunctionAndArray/SpanOfArray.java
@@ -4,7 +4,21 @@ public class SpanOfArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner sc = new Scanner(System.in);
+		   int n = sc.nextInt();
+		   int max = Integer.MIN_VALUE;
+		   int min = Integer.MAX_VALUE;
+		   int a[] = new int[n];
+		   for(int i=0;i<n;i++){
+		       a[i]= sc.nextInt();
+		       if(max<a[i]){
+		           max=a[i];
+		       }
+		       if(min>a[i]){
+		           min=a[i];
+		       }
+		   }
+		   System.out.println(max-min);
 	}
 
 }
