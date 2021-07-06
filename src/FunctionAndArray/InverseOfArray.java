c3bd734f4371d0ecd77cc45d66098b7b2eb97647
Osho Rajneesh <oshorajneesh453@gmail.com>
2021-07-06 21:36:02 +0530

    Updated

--- src/FunctionAndArray/InverseOfArray.java
+++ src/FunctionAndArray/InverseOfArray.java
@@ -1,10 +1,41 @@
 package FunctionAndArray;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class InverseOfArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void display(int[] a){
+		    StringBuilder sb = new StringBuilder();
 
-	}
+		    for(int val: a){
+		    	 sb.append(val + "\n");
+		    }
+		    System.out.println(sb);
+		  }
+
+		  public static int[] inverse(int[] a){
+		    int b[]= new int[a.length];
+		    for(int i=0;i<a.length;i++){
+		        int x =a[i];
+		        b[x]=i;
+		    }
+		    
+		    return b;
+		  }
+
+		public static void main(String[] args) throws Exception {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		    int n = Integer.parseInt(br.readLine());
+		    int[] a = new int[n];
+		    for(int i = 0; i < n; i++){
+		       a[i] = Integer.parseInt(br.readLine());
+		    }
+
+		    int[] inv = inverse(a);
+		    display(inv);
+		 }
+
 
 }
--- src/FunctionAndArray/SubArrayProblem.java
+++ src/FunctionAndArray/SubArrayProblem.java
@@ -1,10 +1,24 @@
 package FunctionAndArray;
 
+import java.util.Scanner;
+
 public class SubArrayProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner sc = new Scanner(System.in);
+		   int n = sc.nextInt();
+		   int a[] = new int[n];
+		   for(int i=0;i<n;i++){
+		       a[i]= sc.nextInt();
+		   }
+		   for(int i=0;i<n;i++){
+		       for(int j=i;j<n;j++){
+		           for(int k=i;k<=j;k++){
+		           System.out.print(a[k]+"	");
+		           }System.out.println();
+		       }
+		   }
 	}
 
 }
