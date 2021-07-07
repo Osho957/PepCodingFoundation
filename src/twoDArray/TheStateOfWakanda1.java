69891dafe3147e9a64b9b184642e0c817801b706
Osho Rajneesh <oshorajneesh453@gmail.com>
2021-07-07 22:03:06 +0530

    Updated

--- src/twoDArray/TheStateOfWakanda1.java
+++ src/twoDArray/TheStateOfWakanda1.java
@@ -1,10 +1,32 @@
 package twoDArray;
 
+import java.util.Scanner;
+
 public class TheStateOfWakanda1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    int a[][] = new int [n][m];
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            a[i][j]= sc.nextInt();
+	        }
+	    }
+	    for(int i=0;i<m;i++){
+	         if(i%2==0){
+	        for(int j=0;j<n;j++){
+	           
+	            System.out.println(a[j][i]);
+	        }
+	        }else{
+	            for(int j=n-1;j>=0;j--){
+	                System.out.println(a[j][i]);
+	            }
+	        }
+	    }
 	}
 
 }
--- src/twoDArray/TwodArrayDemo.java
+++ src/twoDArray/TwodArrayDemo.java
@@ -1,10 +1,25 @@
 package twoDArray;
 
+import java.util.Scanner;
+
 public class TwodArrayDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		   Scanner sc = new Scanner(System.in);
+		   int n = sc.nextInt();
+		   int m = sc.nextInt();
+		   int a[][] = new int[n][m];
+		    for(int i=0;i<n;i++){
+		       for(int j=0;j<m;j++){
+		          a[i][j]=sc.nextInt();
+		       }
+		   }
+		   for(int i=0;i<n;i++){
+		       for(int j=0;j<m;j++){
+		           System.out.print(a[i][j]+" ");
+		       }System.out.println();
+		   }
 	}
 
 }
