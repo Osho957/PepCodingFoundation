a5d65806d2d2ab44ea1912ffdb656db59b871aa6
Osho Rajneesh <oshorajneesh453@gmail.com>
2021-07-15 18:18:47 +0530

    Updated

--- src/StringStringBuilderArraList/StirngWithDiff.java
+++ src/StringStringBuilderArraList/StirngWithDiff.java
@@ -1,10 +1,30 @@
 package StringStringBuilderArraList;
 
+import java.util.Scanner;
+
 public class StirngWithDiff {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static String solution(String str){
+		 String ans ="";
+	   int diff =0;
+	   for(int i=0;i<str.length()-1;i++){
+	       char ch1 = str.charAt(i+1);
+	       char ch2 = str.charAt(i);
+	       diff = ch1-ch2;
+	       ans+=ch2;
+	       ans+=diff;
+	   }
+	   
+	   ans+=str.charAt(str.length()-1);
+
+		return ans;
 	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
 
 }
