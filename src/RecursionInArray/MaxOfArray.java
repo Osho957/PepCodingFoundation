51420507246e26811eb0bb94859a06bebdd25f5f
Osho Rajneesh <oshorajneesh453@gmail.com>
2021-07-21 22:42:12 +0530

    Updated

--- src/RecursionInArray/MaxOfArray.java
+++ src/RecursionInArray/MaxOfArray.java
@@ -1,5 +1,34 @@
 package RecursionInArray;
 
+import java.util.Scanner;
+
 public class MaxOfArray {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i]= sc.nextInt();
+        }
+       System.out.println(maxOfArray(a,0));
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        
+        if(idx==arr.length-1){
+            return arr[idx];
+        }
+        
+        int max =   maxOfArray(arr,idx+1);
+        
+       if(max>arr[idx]){
+           return max;
+       }else{
+           return arr[idx];
+       }
+        
+      
+        
+    }
 
 }
